dabcacc4e629fc313c6e35a5c5a28203bbc042dd
Rafael Ferrari <rafa.ferrari@outlook.com>
2018-06-02 13:35:08 -0300

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=opentheboxbot-Web-Deploy.pubxml -p:Password=DNjttfguEn2hA4pwTS7r5MXquXmpn6bxAx6Fa88ibzwC5vfSaxtsGhnCLohb
+
