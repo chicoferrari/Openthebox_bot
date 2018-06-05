f2c9b8016009346b2f1c40c229bd62ce68915626
Rosemary Sumitani <sumitani.mitsue@gmail.com>
2018-06-05 00:44:01 -0300

    Oops

---
+++ Constants/QuizPathOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot.Constants
+{
+    public static class QuizPathOptions
+    {
+        public const string NetworkingOption = "Redes de Computadores";
+        public const string GeneralOption = "Conhecimentos Gerais";
+    }
+}
---
+++ Models/QuestionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot.Models
+{
+    public class QuestionModel
+    {
+        public int Index { get; set; }
+        public string Text { get; set; }
+        public List<string> Answers { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
